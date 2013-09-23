f76bbe4888a146452bfe2faa5e265418fa9484d0
Luiz Branco <me@luizbranco.com>
2013-09-22 21:57:49 -0300

    Add initial app structure

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require("express");
 var path = require("path");
+var passport = require("passport");
 var app = module.exports = express();
 var port = process.env.PORT || 8080;
 
@@ -10,13 +11,17 @@ app.use(express.static(path.join(__dirname + "/public")));
 app.use(express.cookieParser());
 app.use(express.bodyParser());
 app.use(express.session({secret: process.env.SECRET || "trolololo"}));
+app.use(passport.initialize());
+app.use(passport.session());
 
 /*
  * App Modules
  */
+var auth = require("./lib/auth");
 var home = require("./lib/home");
 var dashboard = require("./lib/dashboard");
 
+var auth = require("./lib/auth");
 app.use(home);
 app.use(dashboard);
 
